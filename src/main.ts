c234a1f999abefde5194d54c27f859734f824e36
chengdev <dong862647669@gmail.com>
2025-01-07 19:55:39 +0800

    chore: unplugin-element-plus自动导入样式

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import './assets/reset.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'element-plus/theme-chalk/index.css'
+// ixmport 'element-plus/theme-chalk/index.css'
 import myPlugin from './plugins'
 import directives from './directives'
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// import AutoImport from 'unplugin-auto-import/vite'
+// import Components from 'unplugin-vue-components/vite'
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
@@ -38,6 +42,13 @@ export default defineConfig({
     plugins: [
         vue(),
         vueJsx(),
+        // AutoImport({
+        //     resolvers: [ElementPlusResolver()]
+        // }),
+        // Components({
+        //     resolvers: [ElementPlusResolver()]
+        // }),
+        ElementPlus({}),
         viteStaticCopy({
             targets: [
                 {
